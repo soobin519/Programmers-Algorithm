81cae6936534e8dbada474aa15c83e0a034b6317
soobin519 <soobin0519@naver.com>
2021-09-02 16:06:58 +0900

    9.2

--- Lv1_비밀지도.py
+++ Lv1_비밀지도.py
@@ -23,3 +23,14 @@ def solution(n, arr1, arr2):
         answer[i]=s
 
     return answer
+
+#solution2
+def solution(n, arr1, arr2):
+    answer = []
+    for i,j in zip(arr1,arr2):
+        a12 = str(bin(i|j)[2:])
+        a12=a12.rjust(n,'0')
+        a12=a12.replace('1','#')
+        a12=a12.replace('0',' ')
+        answer.append(a12)
+    return answer
